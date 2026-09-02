070321e8502631bf5421cc5d31c4d198dafd537b
avanishkasar <avanishkasar@gmail.com>
2026-09-02 09:22:22 +0000

    practice: practiced two sum using hash map approach

---
+++ practice/2026-09-02/snippet_09-22-22_014.py
@@ -0,0 +1,8 @@
+# Practice commit 14
+# Topic: practiced two sum using hash map approach
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
