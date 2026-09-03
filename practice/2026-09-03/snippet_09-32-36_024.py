82445ddda7413ee39b9119035b762661391a16fe
avanishkasar <avanishkasar@gmail.com>
2026-09-03 09:32:36 +0000

    practice: practiced greedy algorithm thinking

---
+++ practice/2026-09-03/snippet_09-32-36_024.py
@@ -0,0 +1,8 @@
+# Practice commit 24
+# Topic: practiced greedy algorithm thinking
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
