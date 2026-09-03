6e21a928496445634c176f46796b738528a6c81d
avanishkasar <avanishkasar@gmail.com>
2026-09-03 09:31:54 +0000

    practice: practiced stack-based problem solving

---
+++ practice/2026-09-03/snippet_09-31-54_013.py
@@ -0,0 +1,8 @@
+# Practice commit 13
+# Topic: practiced stack-based problem solving
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
