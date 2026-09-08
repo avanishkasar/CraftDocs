39298a6451e846c041f115949c4afaf186937b07
avanishkasar <avanishkasar@gmail.com>
2026-09-08 09:26:46 +0000

    practice: studied binary search: how it halves search space

---
+++ practice/2026-09-08/snippet_09-26-46_020.py
@@ -0,0 +1,8 @@
+# Practice commit 20
+# Topic: studied binary search: how it halves search space
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
