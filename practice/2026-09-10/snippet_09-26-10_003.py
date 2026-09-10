36a461b3917456683a19ec6914fda4732723b5cb
avanishkasar <151715253+avanishkasar@users.noreply.github.com>
2026-09-10 09:26:10 +0000

    practice: studied enumerate(): loop with index and value

---
+++ practice/2026-09-10/snippet_09-26-10_003.py
@@ -0,0 +1,8 @@
+# Practice commit 3
+# Topic: studied enumerate(): loop with index and value
+
+stk = []
+for ch in '({[]})':
+    if ch in '({[': stk.append(ch)
+    else: stk.pop() if stk else None
+print(not stk)
